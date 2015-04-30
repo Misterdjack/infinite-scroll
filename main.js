cc43742768b9cabb4b3b4ea05821fbf65ff04a6c
Darren Jack <darrencharlesjack79@gmail.com>
2015-04-30 16:05:30 -0600

    some initial logic

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 
+$(window).on('scroll', function scrollHandler() {
+		var distanceFromBottom = $(document).height() - $(window).scrollTop() - $(window).height()
+		if(distanceFromBottom < $(window).height()) {
+			var lastItemEl = $('.item:last');
+			appendItem(lastItemEl);
+		}
+	});
 
 $(document).on('ready', function() {
   
