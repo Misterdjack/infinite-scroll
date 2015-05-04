b10d38479e284c07b6b6134b406ee579d7c208b7
Darren Jack <darrencharlesjack79@gmail.com>
2015-05-04 17:30:13 -0600

    a lot of craziness i'm playing around with

--- main.js
+++ main.js
@@ -1,27 +1,69 @@
 
 
-var appendItem = function (data) {
-	var item = $('div').append(data);
-	$('.container').append(item);
-	// console.log(data);
+var freebird = function(e){
+	e.preventDefault();
+
+  	$('#myModal').modal('show');
+
+  	var selectedItem = $(this).closest('.item').clone();
+  	var selectedSrc = selectedItem.attr('data-image');
+  	var selectedImage = selectedItem.html('<img class="images" src="' + selectedSrc + '">');
+
+  	console.log(selectedImage);
+  	$('.modal-body').html(selectedImage);
+
 };
 
-$(window).on('scroll', function scrollHandler() {
+var popover = function(e){
+  e.preventDefault();
+
+  $('#popoverOption').popover({ trigger: "hover", html:true});
+
+
+};
+
+
+// Bootstrap popover
+$('.item').popover({ trigger: "hover", html:true});
+
+$('#popoverOption').popover({ trigger: "hover", html:true});
+
+// jQuery event listeners
+$(document).on('ready', function(){
+
+	// Opt in toggle initiallizer 
+	$(function () {
+	  $('[data-toggle="popover"]').popover()
+	})
+
+	// Global variables
+	var containerContents = $(".container").html();
+	var container = '<div class="container">' + containerContents + '</div>';
+
+	$(window).on('scroll', function() {
 		var scrollBottom = $(document).height() - $(window).scrollTop() - $(window).height();
-		// var distanceFromTop = $(document).height() - $(window).scrollTop();
-		if(scrollBottom < $(window).height()) {
-			var lastItemEl = $('.list').clone();
-			console.log(lastItemEl);
-			appendItem(lastItemEl);
+
+		if (scrollBottom == 0 ) {
+			$(".container").last().after(container);
+			if ($(".container").length > 3) {
+				$(".container").first().remove();
+				$(window).scrollTop($(".container").height());
+			}
 		}
-		// else if (distanceFromTop < $(window).height()) {
-		// 	var firstItemEl = $('.item:first');
-		// 	prependItem(firstItemEl);
-		// }
-});
+		else if ( $(window).scrollTop() == 0 ) {
+			$(".container").first().before(container);
+			$(window).scrollTop($(".container").height());
+			if ($(".container").length > 3) {
+				$(".container").last().remove();
+			}
+		}    
+	});
+
+	// Launch modal
+	$(document).on('click', '.item', freebird);
+
+	$(document).on('popover', '#popoverOption', popover);
 
 
 
-// $(document).on('ready', function() {
-  
-// });
+});	
