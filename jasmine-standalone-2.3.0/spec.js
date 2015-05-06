9bad393174eeef618387d10519b91a84816326d1
Darren Jack <darrencharlesjack79@gmail.com>
2015-05-06 11:55:55 -0600

    fixed another version error

--- jasmine-standalone-2.3.0/spec.js
+++ jasmine-standalone-2.3.0/spec.js
@@ -1,9 +1,37 @@
-describe("A suite", function() {
-  
-  // var scrollHandler:
 
-  it("contains spec with an expectation", function() {
-    expect(containers).toBe(0);
+describe('container', function() {
+  
+  // var spyEvent = spyOnEvent('window', 'scroll')
+  // $('window').scroll();
+  // expect('scroll').toHaveBeenTriggeredOn('window')
+  // expect(spyEvent).toHaveBeenTriggered()
+
+  // var spyEvent = spyOnEvent('.item', 'click');
+  // $('.item').click();
+  // expect('click').toHaveBeenTriggeredOn('.item');
+  // expect(spyEvent).toHaveBeenTriggered();
+  // var elem2;
+
+  beforeEach(function(){
+    elem2 = $('<div id="container class="container"><p>Hello World</p></div>')
+  });
+
+  it('detects whether an element has a class', function() {
+    expect(elem2).toBe('#container');
+  });
+
+  var elem;
+  beforeEach(function(){
+    elem = $('.container').length;
+  });
+
+  it('should keep the number of container divs always under 4', function() {
+    scrollHandler();
+    expect(elem).not.toEqual(4);
+  });
+
+  it('should be true', function () {
+    expect(true).toBeTruthy();
   });
 
 });
