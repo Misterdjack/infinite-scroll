686a932dbb030b8bdaa4e5176d7627e6b0197401
Darren Jack <darrencharlesjack79@gmail.com>
2015-05-07 09:53:08 -0600

    some more tests

--- jasmine-standalone-2.3.0/spec.js
+++ jasmine-standalone-2.3.0/spec.js
@@ -1,6 +1,6 @@
 
 
-describe("A spy", function() {
+describe("Testing the freebird function", function() {
 
   var bar = null;
 
@@ -12,7 +12,7 @@ describe("A spy", function() {
     app.freebird(456, 'another param');
   });
 
-  it("tracks that the spy was called", function() {
+  it("tracks that the function was called", function() {
     expect(app.freebird).toHaveBeenCalled();
   });
 
@@ -24,4 +24,66 @@ describe("A spy", function() {
   it("stops all execution on a function", function() {
     expect(bar).toBeNull();
   });
+});
+
+describe("Testing the scrollHandler function", function() {
+
+  var bar = null;
+
+  beforeEach(function() {
+
+    spyOn(app, 'scrollHandler');
+
+    app.scrollHandler(123);
+    app.scrollHandler(456, 'another param');
+  });
+
+  it("tracks that the function was called", function() {
+    expect(app.scrollHandler).toHaveBeenCalled();
+  });
+
+  it("tracks all the arguments of its calls", function() {
+    expect(app.scrollHandler).toHaveBeenCalledWith(123);
+    expect(app.scrollHandler).toHaveBeenCalledWith(456, 'another param');
+  });
+
+  it("stops all execution on a function", function() {
+    expect(bar).toBeNull();
+  });
+});
+
+describe('container', function() {
+  
+  // var spyEvent = spyOnEvent('window', 'scroll')
+  // $('window').scroll();
+  // expect('scroll').toHaveBeenTriggeredOn('window')
+  // expect(spyEvent).toHaveBeenTriggered()
+
+  // var spyEvent = spyOnEvent('.item', 'click');
+  // $('.item').click();
+  // expect('click').toHaveBeenTriggeredOn('.item');
+  // expect(spyEvent).toHaveBeenTriggered();
+  // var elem2;
+
+  beforeEach(function(){
+    elem2 = $('<div id="container class="container"><p>Hello World</p></div>')
+  });
+
+  it('detects whether an element has a class', function() {
+    expect(elem2).toBe('#container');
+  });
+
+  var elem;
+  beforeEach(function(){
+    elem = $('.container').length;
+  });
+
+  it('should keep the number of container divs always under 4', function() {
+    expect(elem).not.toEqual(4);
+  });
+
+  it('should be true', function () {
+    expect(true).toBeTruthy();
+  });
+
 });
